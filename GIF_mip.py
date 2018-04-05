c7027fa2ff774c55c482d7d2081ac65e9a5d494d
paul-bd <paul.blancdurand@gmail.com>
2018-04-05 16:46:50 +0200

    Update GIF_mip.py

--- GIF_mip.py
+++ GIF_mip.py
@@ -34,18 +34,17 @@ def create_gif(filenames, duration):
 
 def create_mipGIF_from_3D(img,nb_image=40,duration=0.1,is_mask=False,borne_max=None):
     ls_mip=[]
-    while len(img.shape)>3:
-        img=nib.squeeze_image(img)
+
     img_data=img.get_data()
     img_data+=1e-5
     
     for angle in np.linspace(0,360,nb_image):
         ls_slice=[]
-        for i in range(img_data.shape[-1]):
-            ls_slice.append(scipy.ndimage.interpolation.rotate(img_data[:,:,i],angle))
-        vol_angle=np.stack(ls_slice,axis=-1)
+        vol_angle= scipy.ndimage.interpolation.rotate(img_data,angle)
+        
         MIP=np.amax(vol_angle,axis=1)
-        MIP[MIP<2*1e-5]=0
+        MIP-=1e-5
+        MIP[MIP<1e-5]=0
         MIP=np.flipud(MIP.T)
         ls_mip.append(MIP)
     try:
@@ -54,24 +53,24 @@ def create_mipGIF_from_3D(img,nb_image=40,duration=0.1,is_mask=False,borne_max=N
         pass
     os.mkdir('test_gif/')
 
-ls_image=[]
-for mip,i in zip(ls_mip,range(len(ls_mip))):
-    fig,ax=plt.subplots()
-    ax.set_axis_off()
-    if borne_max is None:
-        if is_mask==True:
-            borne_max=1
+    ls_image=[]
+    for mip,i in zip(ls_mip,range(len(ls_mip))):
+        fig,ax=plt.subplots()
+        ax.set_axis_off()
+        if borne_max is None:
+            if is_mask==True:
+                borne_max=1
             else:
                 borne_max=15000
-    plt.imshow(mip,cmap='Greys',vmax=borne_max)
+        plt.imshow(mip,cmap='Greys',vmax=borne_max)
         fig.savefig('test_gif/MIP'+'%04d' % (i)+'.png')
         plt.close(fig)
 
-filenames=glob.glob('test_gif/*.png')
+    filenames=glob.glob('test_gif/*.png')
 
-create_gif(filenames, duration)
-try:
-    shutil.rmtree('test_gif/')
+    create_gif(filenames, duration)
+    try:
+        shutil.rmtree('test_gif/')
     except:
         pass
 
